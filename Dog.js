b97f6be36982708983f7ad8ff22680d4b32490ee
LuongLaptop <tdtl.dracula@gmail.com>
2021-07-26 19:53:46 +0700

    test run by node

--- Dog.js
+++ Dog.js
@@ -1,3 +1,11 @@
+var chalk = require('chalk');
 function Dog(name){
-    
+    this.name = name;
+    this.stomach =[];
 }
+
+Dog.prototype.sayHi = function () {
+    console.log("hi, I'm Dog "+chalk.yellow(this.name));
+}
+
+module.exports = Dog;
